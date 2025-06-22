a3965ad42a7c3d9bb6e490e096ce95f02ec28f4c
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-06-22 14:50:17 -0700

    icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import remarkIconShorthand from "./emojis.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ld3z.github.io',
-	base: '/server-releases/',
+  site: "https://ld3z.github.io",
+  base: "/server-releases/",
+
+  markdown: {
+    remarkPlugins: [remarkIconShorthand],
+  },
 });
