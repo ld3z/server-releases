df3808c4ed74a4ba24899d228ee8ba15fa4f3c7a
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-06-22 11:12:07 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# server-releases
+# yo
---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://ld3z.github.io',
+	base: 'server-releases',
+});
